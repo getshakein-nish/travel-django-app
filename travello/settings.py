f95d8e6c4d6c05614441568fa4aac41ed5ac2959
nishant <ktanay7870@gmail.com>
2024-02-04 21:28:06 +0530

    allowed hosts

--- travello/settings.py
+++ travello/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-23sr1f%8v-5w19p=f+z^+%p3e)22wp^ojgjw#gx&$rute3=j*2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
